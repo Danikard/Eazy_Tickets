6d78e2b8da25a8e17aa667c4b883946a4fd3d59d
DaniKard <kajothada@gmail.com>
2020-12-17 13:24:42 +0200

    add method

--- src/main/java/signUpLogin/Db.java
+++ src/main/java/signUpLogin/Db.java
@@ -3,4 +3,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Db {
+    public Connection databaseLink;
+    public getConnection (){
+        String databaseName ="demo_db";
+        String databaseUserName="user";
+        String databasePassword="password";
+        String url="jdbc:mysql://localhost"+ databaseName;
+        try{
+            class.forName("com.mysql.cj.jbbc.driver");
+            databaseLink= DriverManager.getConnection(url,databaseUserName,databasePassword);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+        return databaseLink
+    }
 }
