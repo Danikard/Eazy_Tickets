6d8a5d5c183bcbdbfa9a412063e41b6f40218730
DaniKard <kajothada@gmail.com>
2020-12-17 10:34:48 +0200

    add username  textFeild

--- src/main/java/signUpLogin/Login.java
+++ src/main/java/signUpLogin/Login.java
@@ -1,10 +1,10 @@
+package signUpLogin;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
-import javafx.event.ActionEvent;
-//package signUpLogin;
-//
-//public class Login {
-//}
+
+public class Login {
+    private TextField usernameTextField;
+}
