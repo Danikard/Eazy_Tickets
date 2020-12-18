58fdf831fbcfee1327bbf4dd2a8aacca75de5cfe
DaniKard <kajothada@gmail.com>
2020-12-18 09:58:58 +0200

    remove comments

--- src/main/java/signUpLogin/DB.java
+++ src/main/java/signUpLogin/DB.java
@@ -11,27 +11,3 @@ public class DB {
 
 
 
-//package signUpLogin;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//
-//public class DB {
-//    public Connection databaseLink;
-//    public Connection getConnection (){
-//        String databaseName ="demo_db";
-//        String databaseUserName="user";
-//        String databasePassword="password";
-//        String url="jdbc:mysql://localhost"+ databaseName;
-//        try{
-//            Class.forName("com.mysql.cj.jbbc.driver");
-//            databaseLink= DriverManager.getConnection(url,databaseUserName,databasePassword);
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            e.getCause();
-//        }
-//        return databaseLink;
-//    }
-//
-//
-//}
